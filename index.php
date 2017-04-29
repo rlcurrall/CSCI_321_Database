deab50b607aa2cdd4bc75bb9514f7d6881fcce96
Robert Currall <rlcurrall@gmail.com>
2017-04-29 16:49:46 -0400

    extra commits

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if ( $action == NULL )
     $action = filter_input(INPUT_GET, 'action');
     if ( $action == NULL )
     {
-        $action = 'profile';
+        $action = 'view_characters';
     } // end nested if
 } // end if
 
@@ -54,7 +54,7 @@ switch($action)
         if (is_valid_login($_SESSION['username'], $_SESSION['password']))
         {
             $_SESSION['is_valid_login'] = true;
-            include('view/profile.php');
+            include('view/characters.php');
         }
         else
         {
@@ -68,8 +68,8 @@ switch($action)
         $login_message = 'You have been logged out.';
         include('view/login.php');
         break;
-    case 'profile':
-        include('view/profile.php');
+    case 'view_characters':
+        include('view/characters.php');
         break;
     case 'show_register_form':
         include('view/register.php');
@@ -78,32 +78,83 @@ switch($action)
         $email = filter_input(INPUT_POST, 'email');
         $username = filter_input(INPUT_POST, 'username');
         $password = filter_input(INPUT_POST, 'password');
-        add_user($username, $password, $email);
+        $name = filter_input(INPUT_POST, 'name');
+        add_user($username, $name, $password, $email);
         $_SESSION = array();
         session_destroy();
-        $lgoin_message = "Login using your new username and password.";
+        $login_message = "Login using your new username and password.";
         include('view/login.php');
         break;
     case 'search' :
         // TODO 
         break;
-    case 'view_characters' :
-        // TODO
-        break;
     case 'add_character' :
-        // TODO
+        $userID = filter_input(INPUT_POST, 'user_id');
+        $name = filter_input(INPUT_POST, 'name');
+        $background = filter_input(INPUT_POST, 'background');
+        add_character($userID, $name, $background);
+        header('Location: .');
         break;
     case 'add_character_form':
-        // TODO
+        $user_id = filter_input(INPUT_POST, 'user_id');
+        include('view/add_character_form.php');
+        break;
+    case 'delete_character' :
+        $characterID = filter_input(INPUT_POST, 'character_id');
+        delete_character($characterID);
+        include('view/characters.php');
+        break;
+    case 'edit_character_form' :
+        $characterID = filter_input(INPUT_POST, 'character_id');
+        include('view/edit_character_form.php');
+        break;
+    case 'edit_character' :
+        $characterID = filter_input(INPUT_POST, 'character_id');
+        $name = filter_input(INPUT_POST, 'name');
+        $background = filter_input(INPUT_POST, 'background');
+        edit_character($characterID, $name, $background);
+        header('Location: .');
         break;
     case 'view_games' :
-        // TODO
+        include('view/games.php');
+        break;
+    case 'add_game_form' :
+        $user_id = filter_input(INPUT_POST, 'user_id');
+        include('view/add_game_form.php');
+        break;
+    case 'add_game' :
+        $userID = filter_input(INPUT_POST, 'user_id');
+        $name = filter_input(INPUT_POST, 'name');
+        $background = filter_input(INPUT_POST, 'description');
+        add_game($userID, $name, $background);
+        header("Location: index.php?action=view_games");
+        break;
+    case 'edit_game_form' :
+        $gameID = filter_input(INPUT_POST, 'game_id');
+        include('view/edit_game_form.php');
+        break;
+    case 'edit_game' :
+        $gameID = filter_input(INPUT_POST, 'game_id');
+        $name = filter_input(INPUT_POST, 'name');
+        $description = filter_input(INPUT_POST, 'description');
+        edit_game($gameID, $name, $description);
+        header('Location: index.php?action=view_games');
+        break;
+    case 'delete_game' :
+        $gameID = filter_input(INPUT_POST, 'game_id');
+        delete_game($gameID);
+        include('view/games.php');
+        break;
+    case 'join_game_form' :
+        $characterID = filter_input(INPUT_POST, 'character_id');
+        $character = get_character($characterID);
+        include('view/join_game.php');
         break;
     case 'join_game' :
-        // TODO
-        break;
-    case 'create_game' :
-        // TODO
+        $characterID = filter_input(INPUT_POST, 'character_id');
+        $gameID = filter_input(INPUT_POST, 'game_id');
+        join_game($gameID, $characterID);
+        header('Location: index.php?action=view_characters');
         break;
     case 'create_game_form' :
         // TODO
